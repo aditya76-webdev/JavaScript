4be625437e6ea252a43c82b3e3789fcf7d42dd05
aditya76-webdev <adityajain77356@gmail.com>
2023-11-20 22:57:36 +0530

    ways to define num

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,12 @@
+/* The line `const score = 400;` is declaring a constant variable named `score` and assigning it the
+value of 400. The `const` keyword is used to declare a variable that cannot be reassigned a new
+value once it has been initialized. */
+const score = 400;
+console.log(score);
+/* The line `const balance = new Number(100);` is creating a new instance of the Number object and
+assigning it to the constant variable `balance`. The Number object is a built-in JavaScript object
+that represents numeric values. In this case, it is being used to create a Number object with an
+initial value of 100. */
+const balance = new Number(100); //iss tarah bhi number declare kar sakte hai,,,Here new is the object whose type is number.
+
+console.log(balance);
